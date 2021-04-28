afaf6630cde23c139362a5d54fffd7ed56164b29
Olesia <odobrogor@gmail.com>
2021-04-28 18:14:12 +0300

    ActionSheetCustomization

--- MyPlaces 2.0/NewPlaceViewController.swift
+++ MyPlaces 2.0/NewPlaceViewController.swift
@@ -37,23 +37,32 @@ class NewPlaceViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            
+            let cameraIcon = #imageLiteral(resourceName: "camera")
+            let photoIcon = #imageLiteral(resourceName: "photo")
             //вызываем меню для загрузки фото
             //определяем сам алерт контроллер
             let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             // определяем пользовательские действия
             let camera = UIAlertAction(title: "Camera", style: .default) { _ in
-                //TODO: Choose image picker
                 self.chooseImagePicker(source: .camera)
             }
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
-                //TODO: Choose image picker
                 self.chooseImagePicker(source: .photoLibrary)
             }
+            photo.setValue(photoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+            
+            
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
             //добавлЯем все пользовательские действия в алерт контроллер
             actionSheet.addAction(camera)
             actionSheet.addAction(photo)
             actionSheet.addAction(cancel)
+            
             //вызываем алерт контроллер
             present(actionSheet, animated: true)
         } else {
